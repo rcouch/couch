8facbe5db1accb4e7436acc67ab752b260b754f2
benoitc <bchesneau@gmail.com>
2014-07-07 01:43:12 +0200

    add couch:get/2, couch:get/3 to fetch a document.
    This change add the couch:get/{2,3} function to fetch a document. It is
    supporting all options and offer a simple API to also stream attachments
    so it can be used to stream a document.
    
    Example of usage:
    
    (rcouch@127.0.0.1)1> {ok, Db} = couch:open_db("testdb").
    {ok,{cdb,<<"testdb">>,
             [{user_ctx,{user_ctx,null,[<<"_admin">>],undefined}}]}}
    (rcouch@127.0.0.1)2>
    (rcouch@127.0.0.1)2> couch:get(Db,
    <<"5f480e40beb20ce551d848a8b5000df7">>).
    {ok,{[{<<"_id">>,<<"5f480e40beb20ce551d848a8b5000df7">>},
          {<<"_rev">>,<<"2-6cc0258e6120c8cdff8a3c75930ce542">>},
          {<<"_attachments">>,
           {[{<<"frais_enki.ods">>,
              {[{<<"content_type">>,
                 <<"application/vnd.oasis.opendocument.spreadsheet">>},
                {<<"revpos">>,2},
                {<<"digest">>,<<"md5-6DZZHRxjEI8Qr+yTLpjG5g==">>},
                {<<"length">>,30272},
                {<<"stub">>,true}]}}]}}]}}
    (rcouch@127.0.0.1)3>
    (rcouch@127.0.0.1)3> {ok, {stream, Next}} = couch:get(Db,
    <<"5f480e40beb20ce551d848a8b5000df7">>, [{open_revs, all}, stream]).
    {ok,{stream,#Fun<couch.4.87847453>}}
    (rcouch@127.0.0.1)4> {doc, Doc, Next1} = couch:stream_doc(Next).
    {doc,{[{<<"_id">>,<<"5f480e40beb20ce551d848a8b5000df7">>},
           {<<"_rev">>,<<"2-6cc0258e6120c8cdff8a3c75930ce542">>},
           {<<"_attachments">>,
            {[{<<"frais_enki.ods">>,
               {[{<<"content_type">>,
                  <<"application/vnd.oasis.opendocument.spreadsheet">>},
                 {<<"revpos">>,2},
                 {<<"digest">>,<<"md5-6DZZHRxjEI8Qr+yTLpjG5g==">>},
                 {<<"length">>,30272},
                 {<<"follows">>,true}]}}]}}]},
         #Fun<couch.6.87847453>}
    (rcouch@127.0.0.1)5>
    (rcouch@127.0.0.1)5>
    (rcouch@127.0.0.1)5> {att, Name, AttInfo, Next2} =
    couch:stream_doc(Next1).
    {att,<<"frais_enki.ods">>,
         [{name,<<"frais_enki.ods">>},
          {att_len,30272},
          {disk_len,30272},
          {type,<<"application/vnd.oasis.opendocument.spreadsheet">>},
          {encoding,identity}],
         #Fun<couch.8.87847453>}
    (rcouch@127.0.0.1)6>
    (rcouch@127.0.0.1)6> {att_body, Name, Chunk, Next3} =
    couch:stream_doc(Next2).
    {att_body,<<"frais_enki.ods">>,
              [<<80,75,3,4,20,0,0,8,0,0,88,52,218,68,133,108,57,138,46,
                 0,0,0,46,0,0,...>>,
               [<<52,8,206,212,207,125,241,127,65,183,48,80,111,24,98,
                  201,116,181,196,118,100,19,198,...>>,
                <<201,73,196,40,144,209,130,188,188,211,115,50,83,211,42,
                  42,190,22,60,126,150,236,...>>,
                <<74,89,81,14,76,36,22,19,85,95,12,4,91,52,47,47,72,79,
                  168,219,124,...>>,
                <<253,84,21,6,113,151,86,21,15,181,59,71,117,32,89,45,94,
                  109,60,36,...>>,
                <<198,71,68,233,8,121,55,205,186,61,196,110,68,199,143,28,
                  227,6,210,...>>,
                <<0,0,0,2,0,0,0,0,0,0,0,80,75,3,4,20,0,0,...>>]],
              #Fun<couch.10.87847453>}
    (rcouch@127.0.0.1)7>
    (rcouch@127.0.0.1)7> {att_eof, Name, Next4} = couch:stream_doc(Next3).
    {att_eof,<<"frais_enki.ods">>,#Fun<couch.9.87847453>}
    (rcouch@127.0.0.1)8> couch:stream_doc(Next4).
    eof

--- src/couch.erl
+++ src/couch.erl
@@ -20,7 +20,12 @@
          open_or_create_db/1, open_or_create_db/2,
          delete_db/1,
          all_databases/0, all_databases/2,
-         database_info/1]).
+         database_info/1,
+         ensure_full_commit/1, ensure_full_commit/2]).
+
+
+-export([get/2, get/3,
+         stream_doc/1]).
 
 -include("couch_db.hrl").
 
@@ -38,6 +43,47 @@
                db/0,
                db_options/0,
                db_info/0]).
+
+
+-type docid() :: binary().
+-type rev() :: binary().
+-type update_type() :: replicated_changes | interactive_edit.
+-type doc_options() :: [attachments |
+                        {revs, list()} |
+                        local_seq |
+                        revs_info |
+                        deleted_conflicts |
+                        rev |
+                        {open_revs, all | list()} |
+                        latest |
+                        {atts_since, list()} |
+                        {update_type, update_type()} |
+                        att_encoding_info |
+                        stream |
+                        {timeout, infinity | integer()}].
+
+-type ejson_array() :: [ejson_term()].
+-type ejson_object() :: {[{ejson_key(), ejson_term()}]}.
+
+-type ejson_key() :: binary() | atom().
+
+-type ejson_term() :: ejson_array()
+    | ejson_object()
+    | ejson_string()
+    | ejson_number()
+    | true | false | null.
+
+-type ejson_string() :: binary().
+
+-type ejson_number() :: float() | integer().
+
+-type doc() :: ejson_object().
+
+-export_types([update_type/0,
+               doc_options/0,
+               docid/0,
+               rev/0,
+               doc/0]).
 
 %% @doc return the couch application version
 version() ->
@@ -155,7 +201,6 @@ all_databases() ->
 all_databases(Fun, Acc0) ->
     couch_server:all_databases(Fun, Acc0).
 
-
 %% @doc get database info
 -spec database_info(db()) -> {ok, db_info()} | {error, term()}.
 database_info(Db) ->
@@ -164,6 +209,190 @@ database_info(Db) ->
                 {ok, {Info}}
         end).
 
+%% @doc ensure full commit of docs
+-spec ensure_full_commit(Db::db())
+    -> {ok, StartTime::integer()} | {error, term()}.
+ensure_full_commit(Db) ->
+    ensure_full_commit(Db, undefined).
+
+%% @doc ensure full commit of docs
+-spec ensure_full_commit(Db::db(), RequiredSeq::integer())
+    -> {ok, StartTime::integer()} | {error, term()}.
+ensure_full_commit(Db, RequiredSeq) ->
+    with_db(Db, fun(Db0) ->
+                UpdateSeq = couch_db:get_update_seq(Db0),
+                CommittedSeq = couch_db:get_committed_update_seq(Db0),
+                case RequiredSeq of
+                    undefined ->
+                        couch_db:ensure_full_commit(Db0);
+                    _ when RequiredSeq > UpdateSeq ->
+                        {error, seq_ahead};
+                    _ when RequiredSeq > CommittedSeq ->
+                        couch_db:ensure_full_commit(Db0);
+                    _ ->
+                        {ok, Db0#db.instance_start_time}
+                end
+        end).
+
+-spec get(Db::db(), docid())
+    ->  {ok, doc()} | {ok, [{ok, doc()} | {missing, rev()}]}
+    | {error, term()}.
+get(Db, DocId) ->
+    get(Db, DocId, []).
+
+-spec get(Db::db(), docid(), doc_options())
+    ->  {ok, doc()} | {ok, [{ok, doc()} | {missing, rev()}]}
+    | {error, term()}.
+get(Db, DocId, Options0) ->
+    Options = case couch_util:get_value(atts_since, Options0) of
+        undefined -> Options0;
+        RevList when is_list(RevList) ->
+            [attachments | Options0]
+    end,
+
+    Revs = couch_util:get_value(open_revs, Options, []),
+    Rev = couch_util:get_value(rev, Options, nil),
+    Stream = proplists:get_value(stream, Options, false),
+
+    case Revs of
+        [] ->
+            case couch_doc_open(Db, DocId, Rev, Options) of
+                {ok, #doc{atts=[]}=Doc} ->
+                    {ok, couch_doc:to_json_obj(Doc, Options)};
+                {ok, Doc} when Stream /= true ->
+                    {ok, couch_doc:to_json_obj(Doc, Options)};
+                {ok, Doc} ->
+                    Options1 = [attachments, follows, att_encoding_info
+                                | Options],
+                    {ok, {stream, fun() ->
+                                    stream_docs([Doc], Options1)
+                            end}};
+                Error ->
+                    Error
+            end;
+        _ ->
+            case open_doc_revs(Db, DocId, Revs, Options) of
+                {ok, Results} when Stream /= true ->
+                    Results2 = lists:foldl(fun
+                                ({ok, Doc}, Acc) ->
+                                    JsonDoc = couch_doc:to_json_obj(
+                                            Doc, Options),
+                                    [{ok, JsonDoc} | Acc];
+                                ({{not_found, missing}, RevId}, Acc) ->
+                                    RevStr = couch_doc:rev_to_str(RevId),
+                                    [{missing, RevStr} | Acc]
+                            end, [], Results),
+                    {ok, lists:reverse(Results2)};
+                {ok, Results} ->
+                    Options1 = [attachments, follows, att_encoding_info
+                                | Options],
+                    {ok, {stream, fun() ->
+                                    stream_docs(Results, Options1)
+                            end}};
+                Error ->
+                    Error
+            end
+    end.
+
+
+stream_doc(Next) when is_function(Next) ->
+    Next().
+
+
+stream_docs([], _Options) ->
+    eof;
+stream_docs([{{not_found, missing}, RevId} | Rest], Options) ->
+    RevStr = couch_doc:rev_to_str(RevId),
+    {missing, RevStr, fun() ->
+                stream_docs(Rest, Options)
+        end};
+stream_docs([{ok, #doc{atts=Atts}=Doc} | Rest], Options) ->
+    JsonDoc = couch_doc:to_json_obj(Doc, Options),
+    {doc, JsonDoc, fun() ->
+                stream_attachments(Atts, Rest, Options)
+        end}.
+
+
+stream_attachments([], [], _Options) ->
+    eof;
+stream_attachments([], Docs, Options) ->
+    stream_docs(Docs, Options);
+stream_attachments([Att |Rest], Docs, Options) ->
+    #att{
+        name=Name,
+        att_len=AttLen,
+        disk_len=DiskLen,
+        type=Type,
+        encoding=Encoding
+    } = Att,
+
+    AttInfo = [{name, Name},
+               {att_len, AttLen},
+               {disk_len, DiskLen},
+               {type, Type},
+               {encoding, Encoding}],
+
+    Ref = make_ref(),
+    AttPid = spawn_link(fun() ->
+                    att_loop(Att, Ref)
+            end),
+    {att, Name, AttInfo, fun() ->
+                stream_attachment(Name, Ref, AttPid, Rest, Docs, Options)
+        end}.
+
+stream_attachment(Name, Ref, AttPid, Atts, Docs, Options) ->
+    Timeout = couch_util:get_value(timeout, Options, infinity),
+
+    AttPid ! {Ref, self(), next},
+    receive
+        {Ref, att_eof} ->
+            {att_eof, Name, fun() ->
+                        stream_attachments(Atts, Docs, Options)
+                end};
+        {Ref, Bin} ->
+            {att_body, Name, Bin, fun() ->
+                        stream_attachment(Name, Ref, AttPid, Atts, Docs,
+                                          Options)
+                end};
+
+        {'EXIT', _Pid, Reason} ->
+            {error, Reason}
+    after Timeout ->
+            couch_util:shutdown_sync(AttPid)
+    end.
+
+att_loop(#att{data=Bin}, Ref) when is_binary(Bin) ->
+    receive
+        {Ref, From, next} ->
+            From ! {Ref, Bin}
+    end;
+att_loop(#att{data={Fd,Sp}, md5=Md5}, Ref) ->
+    Ref = couch_stream:foldl(Fd, Sp, Md5, fun att_cb/2, Ref),
+    receive
+        {Ref, From, next} ->
+            From ! {Ref, att_eof}
+    end;
+att_loop(#att{data=DataFun ,att_len=Len}, Ref) when is_function(DataFun) ->
+    Ref = fold_streamed_data(DataFun, Len, fun att_cb/2, Ref),
+    receive
+        {Ref, From, next} ->
+            From ! {Ref, att_eof}
+    end.
+
+
+att_cb(Bin, Ref) ->
+    receive
+        {Ref, From, next} ->
+            From ! {Ref, Bin},
+            Ref
+    end.
+
+fold_streamed_data(_RcvFun, 0, _Fun, Acc) ->
+    Acc;
+fold_streamed_data(RcvFun, LenLeft, Fun, Acc) when LenLeft > 0->
+    Bin = RcvFun(),
+    ResultAcc = Fun(Bin, Acc),
+    fold_streamed_data(RcvFun, LenLeft - size(Bin), Fun, ResultAcc).
 
 %% @private
 dbname(DbName) when is_list(DbName) ->
@@ -173,6 +402,36 @@ dbname(DbName) when is_binary(DbName) ->
 dbname(DbName) ->
     erlang:error({illegal_database_name, DbName}).
 
+
+
+
+open_doc_revs(Db0, DocId, Revs, Options) ->
+    with_db(Db0, fun(Db) ->
+                couch_db:open_doc_revs(Db, DocId, Revs, Options)
+        end).
+
+couch_doc_open(Db0, DocId, Rev, Options) ->
+    with_db(Db0, fun(Db) ->
+                case Rev of
+                    nil -> % open most recent rev
+                        case couch_db:open_doc(Db, DocId, Options) of
+                            {ok, Doc} ->
+                                {ok, Doc};
+                            Error ->
+                                {error, Error}
+                        end;
+                    _ -> % open a specific rev (deletions come back as stubs)
+                        case couch_db:open_doc_revs(Db, DocId, [Rev],
+                                                    Options) of
+                            {ok, [{ok, Doc}]} ->
+                                {ok, Doc};
+                            {ok, [{{not_found, missing}, Rev}]} ->
+                                {error, not_found};
+                            {ok, [Else]} ->
+                                {error, Else}
+                        end
+                end
+        end).
 
 with_db(#cdb{name=DbName, options=Options}, Fun) ->
     case couch_server:open(DbName, Options) of
